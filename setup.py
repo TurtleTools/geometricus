3f2047b34fec188f620f78fcf543c8d0e4d22c29
Ninjani <48680156+Ninjani@users.noreply.github.com>
2020-08-09 18:54:06 +0200

    refactor(API): re-export necessary members of modules to make a more accessible public API

--- setup.py
+++ setup.py
@@ -1,42 +1,42 @@
 from setuptools import setup
 
-descr = """geometricus: Rotation and translation invariant alignment-free fingerprints for protein structures. 
-Can be used to generate input for machine learning methods."""
+descr = """geometricus: Rotation and translation invariant alignment-free fingerprints for protein structures."""
 
-DISTNAME = 'geometricus'
-DESCRIPTION = 'Fast, structure-based, alignment-free protein embedding'
+DISTNAME = "geometricus"
+DESCRIPTION = "Fast, structure-based, alignment-free protein embedding"
 LONG_DESCRIPTION = descr
-MAINTAINER = 'Janani Durairaj, Mehmet Akdel'
-MAINTAINER_EMAIL = 'janani.durairaj@wur.nl'
-URL = 'https://github.com/TurtleTools/geometricus'
-LICENSE = 'MIT License'
-DOWNLOAD_URL = 'https://github.com/TurtleTools/geometricus'
-VERSION = '0.0.1'
+MAINTAINER = "Janani Durairaj, Mehmet Akdel"
+MAINTAINER_EMAIL = "janani.durairaj@wur.nl"
+URL = "https://github.com/TurtleTools/geometricus"
+LICENSE = "MIT License"
+DOWNLOAD_URL = "https://github.com/TurtleTools/geometricus"
+VERSION = "0.0.1"
 PYTHON_VERSION = (3, 7)
 INST_DEPENDENCIES = ["scipy", "numba", "prody"]
 
-if __name__ == '__main__':
-    setup(name=DISTNAME,
-          version=VERSION,
-          url=URL,
-          description=DESCRIPTION,
-          long_description=LONG_DESCRIPTION,
-          author=MAINTAINER,
-          author_email=MAINTAINER_EMAIL,
-          license=LICENSE,
-          classifiers=[
-              'Intended Audience :: Science/Research',
-              'License :: OSI Approved :: MIT License',
-              'Programming Language :: Python',
-              'Programming Language :: Python :: 3',
-              'Programming Language :: Python :: 3.7',
-              'Topic :: Scientific/Engineering',
-              'Operating System :: Microsoft :: Windows',
-              'Operating System :: POSIX',
-              'Operating System :: Unix',
-              'Operating System :: MacOS',
-          ],
-          packages=['geometricus'],
-          package_data={},
-          install_requires=INST_DEPENDENCIES,
-          )
+if __name__ == "__main__":
+    setup(
+        name=DISTNAME,
+        version=VERSION,
+        url=URL,
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        author=MAINTAINER,
+        author_email=MAINTAINER_EMAIL,
+        license=LICENSE,
+        classifiers=[
+            "Intended Audience :: Science/Research",
+            "License :: OSI Approved :: MIT License",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 3.7",
+            "Topic :: Scientific/Engineering",
+            "Operating System :: Microsoft :: Windows",
+            "Operating System :: POSIX",
+            "Operating System :: Unix",
+            "Operating System :: MacOS",
+        ],
+        packages=["geometricus"],
+        package_data={},
+        install_requires=INST_DEPENDENCIES,
+    )
