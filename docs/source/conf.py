479e2529db1017b6a06101514c1be47cd4e16c9d
Ninjani <48680156+Ninjani@users.noreply.github.com>
2020-12-13 01:53:32 +0100

    fixing docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -39,6 +39,7 @@ extensions = ["sphinx.ext.autodoc",
               'sphinx.ext.viewcode',
               'sphinx.ext.githubpages',
               "sphinx.ext.napoleon",
+              "nbsphinx",
               ]
 
 # Add any paths that contain templates here, relative to this directory.
